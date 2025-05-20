4dcd94c2979d7114f3e7b4458a0e7ca8244d3107
Joshua <cesteraime@gmail.com>
2025-05-19 23:56:27 -0700

    update jestSetupFile.ts

--- recipe-app/utils/jestSetupFile.ts
+++ recipe-app/utils/jestSetupFile.ts
@@ -1,19 +1,3 @@
 jest.mock("@react-native-async-storage/async-storage", () =>
-    require("@react-native-async-storage/async-storage/jest/async-storage-mock")
+    require("@react-native-async-storage/async-storage/jest/async-storage-mock"),
 );
-
-jest.mock("expo-router", () => {
-    // Cast React to the correct type so TS knows about Fragment
-    const React = require("react");
-
-    return {
-        Stack: (props) => <React.Fragment>{ props.children } </React.Fragment>,
-        useRouter: () => ({
-            back: jest.fn(),
-            push: jest.fn(),
-            replace: jest.fn(),
-            setParams: jest.fn(),
-            getState: jest.fn(),
-        }),
-    };
-});
